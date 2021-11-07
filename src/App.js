52e600a99bc3128e255e5d0105bd20ddab511547
Onur AKSAR <onuraksar96@gmail.com>
2021-11-07 22:06:01 +0300

    app finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+import { useState, useEffect, useRef } from "react";
+
+
+// import CoordinatesTable from './components/CoordinatesTable.js';
+
+
+const App =() => {
+
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const [positions, setPositions] = useState({x: '', y: ''});
+
+  const [isDisplay, setIsDisplay] = useState(true);
+
+  const getPosition = (event) => {
+    setPositions({x: Math.round(event.clientX -ref.current.getBoundingClientRect().left ) , y: Math.round(event.clientY - ref.current.getBoundingClientRect().top) })
+  }
+
+  const clearPosition = () => {
+    setPositions({x: '', y: ''})
+  }
+
+  const ref = useRef(null);
+  
+  const handleResize = () => {
+    let windowSize = window.innerWidth;
+    windowSize > 768 ? setIsDisplay(true) : setIsDisplay(false);
+  }
+  useEffect(()=> {
+    window.addEventListener('resize', handleResize);
+
+  }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {isDisplay && <div>
+      <div className="container">
+        <h1>Image Coordinate App</h1>
+        <h2>Upload an Image and Hover on it!</h2>
+        <div className="image__wrapper">
+          {selectedImage && (
+            <div className="image__container">
+              <img ref={ref} onMouseMove={getPosition} onMouseLeave={clearPosition} alt="not fount" src={URL.createObjectURL(selectedImage)} />
+              {/* <button onClick={() => console.log("width:", ref.current.getBoundingClientRect().width)} > </button> */}
+            </div>
+          )}
+          <label className="custom-file-upload">
+            Choose File
+            <input 
+              type="file"
+              name="image"
+              onChange={(event) => {
+                setSelectedImage(event.target.files[0]);
+              }}
+            />
+          </label>
+
+
+        </div>
+      </div>
+      <div className="coordinates__container">
+          <div className="coordinates__item">X Coordinates: <span>{positions.x}</span> </div>
+          <div  className="coordinates__item" >Y Coordinates: <span>{positions.y}</span>  </div>
+      </div>
+    </div>
+    }
+
+    {!isDisplay && (
+      <div className="error-message">Sorry, This app is available only for screens largers than 768px at the moment...</div>
+    )}
     </div>
   );
 }
